4949311a441e5f77264e7f32a9f936878208d5f4
Adesh <120489531+adesh9425@users.noreply.github.com>
2023-03-22 15:29:29 +0530

    Update loan_schedule.sql

--- loan_schedule.sql
+++ loan_schedule.sql
@@ -1,6 +1,9 @@
 --  loan schedual
          
- (select  ls.loan_id ,ls.emi_number ,ls.dpd ,ls.due_date,ls.last_payment_date,ls.payment_status  ,ls.principal_due ,ls.interest_due,
+
+(
+
+ select  ls.loan_id ,ls.emi_number ,ls.dpd ,ls.due_date,ls.last_payment_date,ls.payment_status  ,ls.principal_due ,ls.interest_due,
  ls.payment_received -( case when (ls.interest_due-ls.payment_received) >0 then ls.payment_received else ls.interest_due  end )
  as principal_paid,( case when (ls.interest_due-ls.payment_received) >0 then ls.payment_received else ls.interest_due  end ) as interest_paid,
  ls.payment_received , 
@@ -12,17 +15,17 @@ ls.principal_due +ls.interest_due -ls.payment_received as payment_left,
 
   order by ls.loan_id,ls.emi_number
   )
-  union 
-  (
-  select vled.loan_id ,vled.emi_number ,vled.dpd ,vled.due_date,ls.payment_date,vled.payment_status  ,ls.principal_due ,ls.interest_due,
- ls.payment_received -( case when (ls.interest_due-ls.payment_received) >0 then ls.payment_received else ls.interest_due  end )
- as principal_paid,( case when (ls.interest_due-ls.payment_received) >0 then ls.payment_received else ls.interest_due  end ) as interest_paid,
- ls.payment_received , 
-ls.principal_due +ls.interest_due -ls.payment_received as payment_left,
- ls.total_closing_balance as total_open_balance,ls.total_closing_balance-( case when (ls.principal_due -ls.payment_received) >0 then
- ls.payment_received else  ls.payment_received -( case when (ls.interest_due-ls.payment_received) >0 then ls.payment_received else ls.interest_due  end )  end )  as total_closing_balance
-  
-  from v3_loan_emis_data vled join v3_loans vl on vl.id=vled.loan_id 
-  where loan_id=4812002 and vled.row_status ='active'
-  )
-  
+  union all
+(
+select vled.loan_id,vled.emi_number ,vl.dpd,vled.due_date ,vled.payment_date as last_payment_date  ,vled.payment_status ,vled.principal_payment_schedule as pricple_due ,
+vled.cumulative_interest_accrued  as interest_due,
+vled.payment_amount  -( case when (vled.cumulative_interest_accrued-vled.payment_amount) >0 then vled.payment_amount else vled.cumulative_interest_accrued  end )
+ as principal_paid,
+( case when (vled.cumulative_interest_accrued-vled.payment_amount) >0 then vled.payment_amount else vled.cumulative_interest_accrued end ) as interest_paid,
+vled.payment_amount ,
+vled.principal_payment_schedule  +vled.cumulative_interest_accrued -vled.payment_amount as payment_left,
+vled.principal_open_balance_schedule as total_open_balance, vled.principal_open_balance_schedule-vled.payment_amount  as total_closing_balance
+from v3_loan_emis_data vled join v3_loans vl on vl.id=vled.loan_id 
+where vl.product_type ='ruby' and vled.row_status ='active' order by vled.loan_id,vled.emi_number
+)
+
